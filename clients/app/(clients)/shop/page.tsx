553c337c561fadafdab317bd9eefe8d91f2721bd
Emmrex1 <oyewooyewale94@gmail.com>
2025-08-25 15:07:41 +0100

    shop

--- clients/app/(clients)/shop/page.tsx
+++ clients/app/(clients)/shop/page.tsx
@@ -1,14 +1,17 @@
-
-import Shop from "@/components/Shop";
 import { getAllBrands, getCategories } from "@/sanity/queries";
-import React from "react";
+import React, { Suspense } from "react";
+import Shop from "@/components/Shop";
 
 const ShopPage = async () => {
   const categories = await getCategories();
   const brands = await getAllBrands();
+
   return (
     <div className="bg-white">
-      <Shop categories={categories} brands={brands} />
+      {/* ✅ Wrap Shop (client component w/ useSearchParams) inside Suspense */}
+      <Suspense fallback={<p>Loading shop...</p>}>
+        <Shop categories={categories} brands={brands} />
+      </Suspense>
     </div>
   );
 };
